51e5b85750dffde96e8d14e3fb76f5131e31724d
root <root@nginx-master.(none)>
2017-06-08 10:57:16 +0800

    archer 更新

--- archer/startup.sh
+++ archer/startup.sh
@@ -5,3 +5,4 @@ ip=${2:-"192.168.1.12"}
 port=${3:-8888}
 
 gunicorn -w 2 --env DJANGO_SETTINGS_MODULE=${settings} --error-logfile=/tmp/archer.err -b ${ip}:${port} archer.wsgi:application
+hello world
